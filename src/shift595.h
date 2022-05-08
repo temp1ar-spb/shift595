6bc976df231a596b8a19c8235ad0c6954bc7bbd3
temp1ar-spb <104315396+temp1ar-spb@users.noreply.github.com>
2022-05-08 16:17:05 +0300

    Update shift595.h
    fixes

--- src/shift595.h
+++ src/shift595.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 
-class shift595 
+class shift595
 {
 public:
     shift595(const uint8_t dataPin, const uint8_t clockPin, const uint8_t latchPin, const uint8_t chipCount);
@@ -9,8 +9,8 @@ public:
     void setAll(uint64_t values);
     void update();
     void set(const uint8_t pin, bool value);
-    void getAll();
-    void get (uint8_t pin);
+    uint64_t getAll();
+    bool get (uint8_t pin);
     void setAllLow();
     void setAllHigh(); 
  
